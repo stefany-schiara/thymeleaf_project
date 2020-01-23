587afd3c8a639f173659f2b6bd74631df11c6135
Stefany Schiara <esquiara@gmail.com>
2020-01-22 22:26:36 -0200

    primeiro commit do projeto

---
+++ src/main/java/br/com/schiara/MvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package br.com.schiara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcThymeleafApplication.class, args);
+	}
+
+}
